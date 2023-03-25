b1199b457333cf07af5e349230cce25e576dcd47
moon <atolow@gmail.com>
2023-03-25 20:30:32 +0900

    wow

---
+++ src/Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    public String title;
+    public int price;
+    public String company;
+    public String autor;
+    public int page;
+    public String isbn;
+}
---
+++ src/FourArithmetic.java
@@ -0,0 +1,12 @@
+public class FourArithmetic {
+    public static void main(String[] args) {
+        int a,b;
+        a=12;
+        b=2;
+        java.lang.String resutl;
+        System.out.println("a+b="+(a+b));
+        System.out.println("a+b="+(a-b));
+        System.out.println("a+b="+(a*b));
+        System.out.println("a+b="+(a/b));
+    }
+}
