75cb88b9d693b987b7686c7503173783cfc01cc3
atolow <104044684+atolow@users.noreply.github.com>
2023-03-26 18:35:58 +0900

    Delete src/fc/java directory

--- src/fc/java/model/CarDAO.java
+++
@@ -1,7 +0,0 @@
-package fc.java.model;
-
-public class CarDAO {
-    public void carInsert(CarDTO car){
-        System.out.println("car정보가 DB에 저장이 되었습니다.");
-    }
-}
--- src/fc/java/model/MyUtil1.java
+++
@@ -1,8 +0,0 @@
-package fc.java.model;
-
-public class MyUtil1 {
-    public int hap(int a, int b){
-        int v=a+b;
-        return v;
-    }
-}
