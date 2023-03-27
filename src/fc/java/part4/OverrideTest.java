43be4bed5c88c141544d4141da795b74d6fd3fa7
atolo <atolow@gmail.com>
2023-03-28 00:20:13 +0900

    2 day

---
+++ src/fc/java/part4/OverrideTest.java
@@ -0,0 +1,16 @@
+package fc.java.part4;
+
+import fc.java.model.Animal;
+import fc.java.model.Cat;
+import fc.java.model.Dog;
+
+public class OverrideTest {
+    public static void main(String[] args) {
+        Animal ani=new Dog();
+        ani.eat();
+
+        ani=new Cat();
+        ani.eat();
+
+    }
+}
