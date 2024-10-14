3cf24431f27c21fc13b11800cf13a28f6254f841
haewook Lee <atolow@gmail.com>
2024-10-14 17:29:26 +0900

    10/12test3

---
+++ Firstjava/src/week05/Main.java
@@ -0,0 +1,14 @@
+package week05;
+
+public class Main {
+    public static void main(String[] args) {
+//        TestThread thread = new TestThread();
+//        thread.start();
+
+//        Runnable run = new TestThread();
+//        Thread thread2 =new Thread(run);
+//        thread2.start();
+
+
+    }
+}
---
+++ Firstjava/src/week05/TestThread.java
@@ -0,0 +1,10 @@
+package week05;
+
+public class TestThread extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.print("*");
+        }
+    }
+}
