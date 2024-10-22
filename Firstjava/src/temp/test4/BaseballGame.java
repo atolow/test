893ce46aa4ff47bef0d50a79b67fc687dfa4a842
haewook Lee <atolow@gmail.com>
2024-10-22 16:22:17 +0900

    자릿수구현

--- Firstjava/src/temp/test4/BaseballGame.java
+++ Firstjava/src/temp/test4/BaseballGame.java
@@ -1,21 +1,22 @@
-package temp.test3;
+package temp.test4;
 
 import java.util.*;
 
 public class BaseballGame {
+    Scanner sc = new Scanner(System.in);
     private Set<Integer> randomset = new LinkedHashSet<>();
     private LinkedList<Integer> result = new LinkedList<>();
     private Set<Integer> characterset = new LinkedHashSet<>();
     private LinkedList<Integer> select = new LinkedList<>();
     private final LinkedList<Record> sample = new LinkedList<>();
     private int gameNumber=1;
-
+    private static Integer change= null;
 
     // 객체 생성시 정답을 만들도록 함
     public BaseballGame() {
         Random random = new Random();
-
-
+        result.clear();
+        randomset.clear();
         while (!(randomset.size() == 3)) {
             int num = random.nextInt(9) + 1;
             randomset.add(num);
@@ -26,10 +27,46 @@ public class BaseballGame {
 
         System.out.println("value = " + result);
     }
-    public void play() {
-        Scanner sc = new Scanner(System.in);
-        BaseballGameDisplay baseballGameDisplay = new BaseballGameDisplay();
+    public BaseballGame(String input) {
+        Random random = new Random();
+        result.clear();
+        randomset.clear();
 
+        while (!(randomset.size() == Integer.parseInt(input))) {
+            int num = random.nextInt(9) + 1;
+            randomset.add(num);
+        }
+        result.addAll(randomset);
+
+        Collections.shuffle(result);
+
+        System.out.println("value = " + result);
+
+    }
+
+    public void level(){
+        System.out.println("설정하고자 하는 자리수를 입력하세요.");
+
+        while(true) {
+            String input = sc.nextLine();
+            change = Integer.parseInt(input);
+            if(change ==3 || change==4 || change==5){
+                break;
+            }else{
+                System.out.println("잘못된 값 입력입니다.");
+            }
+        }
+        System.out.println(change + "난이도로 설정하였습니다.");
+        new BaseballGame(String.valueOf(change)).play();
+
+
+
+    }
+    public void play() {
+        BaseballGameDisplay baseballGameDisplay = new BaseballGameDisplay();
+        if(change==null){
+            change=3;
+        }
 
         int score=0;
 
@@ -48,12 +85,21 @@ public class BaseballGame {
 
 
 
-            // 3. 게임 진행횟수 증가
 
-            // 4. 스트라이크 개수 계산
             int str = countStrike(num);
-            // 5. 정답여부 확인, 만약 정답이면 break 를 이용해 반복문 탈출
             if (str == 3) {
+                System.out.println("정답을 맞히셨습니다.");
+                System.out.println();
+                select.clear();
+                break;
+            }
+            else if(str==4){
+                System.out.println("정답을 맞히셨습니다.");
+                System.out.println();
+                select.clear();
+                break;
+            }
+            else if(str==5){
                 System.out.println("정답을 맞히셨습니다.");
                 System.out.println();
                 select.clear();
@@ -70,11 +116,8 @@ public class BaseballGame {
 
             score++;
 
-            Record record = new Record(score,gameNumber);
+            Record record = new Record(score,gameNumber,change);
             sample.add(record);
-
-
-
         }
         // 게임 진행횟수 반환
         gameNumber++;
@@ -82,7 +125,7 @@ public class BaseballGame {
     }
 
     protected boolean validateInput(String input) {
-        if (input.length() != 3) {
+        if (input.length() != Integer.parseInt(String.valueOf(input.length()))) {
             return false;
         }
         for (char a : input.toCharArray()) { //toCharArray = String을 char형을 배열로 바꿔줌
@@ -102,7 +145,7 @@ public class BaseballGame {
 
     public int countStrike(String input) {
         int strike = 0;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < Integer.parseInt(String.valueOf(input.length())); i++) {
             if (select.get(i) == result.get(i)) {
                 strike++;
             }
@@ -113,7 +156,7 @@ public class BaseballGame {
 
     public int countBall(String input) {
         int ball = 0;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < Integer.parseInt(String.valueOf(input.length())); i++) {
             if (select.get(i) != result.get(i) && select.contains(result.get(i))) {
                 ball++;
             }
--- Firstjava/src/temp/test4/Main.java
+++ Firstjava/src/temp/test4/Main.java
@@ -1,4 +1,4 @@
-package temp.test3;
+package temp.test4;
 
 import java.util.Scanner;
 
@@ -11,10 +11,13 @@ public class Main {
 
         while(true) {
             System.out.println("환영합니다! 원하시는 번호를 입력해주세요");
-            System.out.println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
+            System.out.println("0. 자리수 설정 1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
 
             int game = sc.nextInt();
 
+            if(game==0){
+                baseballGame.level();
+            }
             if(game==1) {
                 baseballGame.play();
             }
